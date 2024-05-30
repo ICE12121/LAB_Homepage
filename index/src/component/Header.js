19febbc8d75da0efe5e1c30f868a66a4df648a31
ICE12121 <iceattpon12@gmail.com>
2024-05-30 09:39:35 +0900

    I added bootstrap.bundle.min

--- index/src/component/Header.js
+++ index/src/component/Header.js
@@ -1,6 +1,7 @@
 // Header.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 // import Container from 'react-bootstrap/Container';
 // import Nav from 'react-bootstrap/Nav';
 // import Navbar from 'react-bootstrap/Navbar';
